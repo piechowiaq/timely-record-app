c26b056f433c34407f0919c28bc18179a9d18db4
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-10-27 18:32:37 +0200

    Refactoring user creation

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Icons from '@/Components/Icons.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+
+            <Icons :name="'logo'"  class="w-20 h-20"/>
+            <p class="ml-2 text-2xl font-bold whitespace-nowrap tracking-widest text-gray-600"><span
+                class="text-cyan-600 ">TIMELY</span> RECORD</p>
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,280 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import Icons from '@/Components/Icons.vue';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen  bg-center bg-gray-100 dark:bg-gray-900 selection:bg-gray-500 selection:text-white"
+    >
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                >Dashboard</Link
+            >
+
+            <template v-else>
+                <Link
+                    :href="route('login')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                    >Log in</Link
+                >
+
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                    >Register</Link
+                >
+            </template>
+        </div>
+
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="px-10">
+                <div class="flex flex-col md:flex-row justify-center md:justify-start items-center">
+                    <Icons :name="'logo'"  class="w-20 h-20"/>
+                    <p class="ml-2 text-2xl font-bold whitespace-nowrap tracking-widest text-gray-600"><span
+                        class="text-cyan-600 ">TIMELY</span> RECORD</p>
+                </div>
+                <div class="mt-8 text-xl dark:text-white">
+                    Welcome to your Timely Record application!
+                </div>
+                <div class="mt-6 text-gray-500 text-justify">
+                    "Timely Record - Zapis na czas" to innowacyjna aplikacja przeznaczona do zarządzania i monitorowania dokumentacji w obiektach hotelowych, aby zapewnić, że wszystkie ważne dokumenty i rekordy są aktualizowane i nie przeterminowane. Aplikacja koncentruje się na automatyzacji przepływu pracy i upewnieniu się, że wszelka dokumentacja, tak jak umowy, certyfikaty czy inne ważne dokumenty są aktualizowane zgodnie z wymaganiami prawnymi oraz standardami branży hotelowej.
+                </div>
+            </div>
+
+            <div class="mt-8">
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                    <div class="p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex transition-all duration-250 focus:outline focus:outline-2 focus:outline-gray-500">
+                        <div>
+                            <div class="h-16 w-16 bg-gray-50 dark:bg-red-800/20 flex items-center justify-center ">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    class="w-7 h-7 stroke-gray-500"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                                    />
+                                </svg>
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Laravel has wonderful documentation covering every aspect of the framework. Whether you
+                                are a newcomer or have prior experience with Laravel, we recommend reading our
+                                documentation from beginning to end.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex transition-all duration-250 focus:outline focus:outline-2 focus:outline-gray-500">
+
+                    <div>
+                            <div
+                                class="h-16 w-16 bg-gray-50 dark:bg-red-800/20 flex items-center justify-center"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    class="w-7 h-7 stroke-gray-500"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+                                    />
+                                </svg>
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript
+                                development. Check them out, see for yourself, and massively level up your development
+                                skills in the process.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex transition-all duration-250 focus:outline focus:outline-2 focus:outline-gray-500">
+                        <div>
+                            <div
+                                class="h-16 w-16 bg-gray-50 dark:bg-red-800/20 flex items-center justify-center "
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    class="w-7 h-7 stroke-gray-500"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
+                                    />
+                                </svg>
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel News</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Laravel News is a community driven portal and newsletter aggregating all of the latest
+                                and most important news in the Laravel ecosystem, including new package releases and
+                                tutorials.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div
+                        class="p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex transition-all duration-250 focus:outline focus:outline-2 focus:outline-gray-500"
+                    >
+                        <div>
+                            <div
+                                class="h-16 w-16 bg-gray-50 dark:bg-red-800/20 flex items-center justify-center "
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    class="w-7 h-7 stroke-gray-500"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64"
+                                    />
+                                </svg>
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Laravel's robust library of first-party tools and libraries, such as
+                                <a
+                                    href="https://forge.laravel.com"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Forge</a
+                                >,
+                                <a
+                                    href="https://vapor.laravel.com"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Vapor</a
+                                >,
+                                <a
+                                    href="https://nova.laravel.com"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Nova</a
+                                >, and
+                                <a
+                                    href="https://envoyer.io"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Envoyer</a
+                                >
+                                help you take your projects to the next level. Pair them with powerful open source
+                                libraries like
+                                <a
+                                    href="https://laravel.com/docs/billing"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Cashier</a
+                                >,
+                                <a
+                                    href="https://laravel.com/docs/dusk"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Dusk</a
+                                >,
+                                <a
+                                    href="https://laravel.com/docs/broadcasting"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Echo</a
+                                >,
+                                <a
+                                    href="https://laravel.com/docs/horizon"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Horizon</a
+                                >,
+                                <a
+                                    href="https://laravel.com/docs/sanctum"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Sanctum</a
+                                >,
+                                <a
+                                    href="https://laravel.com/docs/telescope"
+                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-gray-500"
+                                    >Telescope</a
+                                >, and more.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-8 px-6 sm:items-center sm:justify-between">
+                <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
+                    <div class="flex items-center gap-4">
+                        <div>
+                            <a href="https://linkedin.com/company/timely-record/" target="_blank">
+                                <svg class="fill-gray-500 hover:fill-cyan-600" xmlns="http://www.w3.org/2000/svg"
+                                     width="24" height="24" viewBox="0 0 24 24">
+                                    <path
+                                        d="M20.47 2H3.53a1.45 1.45 0 0 0-1.47 1.43v17.14A1.45 1.45 0 0 0 3.53 22h16.94a1.45 1.45 0 0 0 1.47-1.43V3.43A1.45 1.45 0 0 0 20.47 2ZM8.09 18.74h-3v-9h3ZM6.59 8.48a1.56 1.56 0 1 1 0-3.12a1.57 1.57 0 1 1 0 3.12Zm12.32 10.26h-3v-4.83c0-1.21-.43-2-1.52-2A1.65 1.65 0 0 0 12.85 13a2 2 0 0 0-.1.73v5h-3v-9h3V11a3 3 0 0 1 2.71-1.5c2 0 3.45 1.29 3.45 4.06Z"/>
+                                </svg>
+                            </a>
+                        </div>
+                        <div class="ml-2">
+                            <a href="https://facebook.com/people/Timely-Record/61550085031784/" target="_blank">
+                                <svg class="fill-gray-500 hover:fill-cyan-600" xmlns="http://www.w3.org/2000/svg"
+                                     width="24" height="24" viewBox="0 0 24 24">
+                                    <path id="feFacebook2"
+                                          d="M15.725 22v-7.745h2.6l.389-3.018h-2.99V9.31c0-.874.243-1.47 1.497-1.47h1.598v-2.7a21.391 21.391 0 0 0-2.33-.12c-2.304 0-3.881 1.407-3.881 3.99v2.227H10v3.018h2.607V22H3.104C2.494 22 2 21.506 2 20.896V3.104C2 2.494 2.494 2 3.104 2h17.792C21.506 2 22 2.494 22 3.104v17.792c0 .61-.494 1.104-1.104 1.104h-5.171Z"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                    <Link
+                        :href="route('dashboard')"
+                        class="text-xs text-gray-600 hover:text-cyan-700 rounded-md "
+                        :class="{ isDisabled: $page.props.route === 'contact' }"
+                        preserve-scroll
+                    >
+                        contact@timelyrecord.com
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
