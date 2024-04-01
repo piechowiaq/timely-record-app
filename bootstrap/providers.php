b8468e6f886467d91025ad1c9b8ea0895982c6c4
Shift <shift@laravelshift.com>
2024-04-01 12:44:25 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
+    App\Providers\TestingServiceProvider::class,
 ];
