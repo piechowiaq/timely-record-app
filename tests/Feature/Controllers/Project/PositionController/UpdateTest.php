d020a0038f61070715fa004200cc4aac5e2259af
Bartosz Piechowiak <piechowiaq@gmail.com>
2024-06-20 08:12:54 +0200

    Working on position update

--- tests/Feature/Controllers/Project/PositionController/UpdateTest.php
+++ tests/Feature/Controllers/Project/PositionController/UpdateTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Department;
+use App\Models\Position;
 use App\Models\User;
 use Database\Seeders\RolesAndPermissionsSeeder;
 
@@ -9,13 +10,13 @@ use function Pest\Laravel\patch;
 
 beforeEach(function () {
     $this->validData = fn () => [
-        'name' => 'Kitchen',
+        'name' => 'Executive Chef',
     ];
 });
 
 it('requires authentication', function () {
 
-    patch(route('departments.update', Department::factory()->create(
+    patch(route('positions.update', Position::factory()->create(
     )))
         ->assertRedirect(route('login'));
 
@@ -32,13 +33,13 @@ it('requires authorization', function () {
         $user->assignRole($role);
 
         actingAs($user)
-            ->put(route('departments.update', Department::factory()->create()))
+            ->put(route('positions.update', Position::factory()->create()))
             ->assertForbidden();
     }
 
 });
 
-it('updates a department', function () {
+it('updates a position', function () {
 
     $this->seed(RolesAndPermissionsSeeder::class);
 
@@ -46,14 +47,14 @@ it('updates a department', function () {
     $user->assignRole('admin');
     session(['project_id' => $user->project_id]);
 
-    $department = Department::factory()->create(['project_id' => $user->project_id]);
+    $position = Position::factory()->create(['project_id' => $user->project_id]);
 
-    $departmentData = value($this->validData);
+    $positionData = value($this->validData);
 
-    actingAs($user)->patch(route('departments.update', $department->id), $departmentData);
+    actingAs($user)->patch(route('positions.update', $position->id), $positionData);
 
     $this->assertDatabaseHas(Department::class, [
-        ...$departmentData,
+        ...$positionData,
         'project_id' => $user->project_id,
     ]);
 });
